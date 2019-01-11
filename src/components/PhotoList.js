ad8570b05a2080182ffb2469867693f9b939cf02
elizarova <elizarova.atl@gmail.com>
2019-01-11 11:41:23 +0300

    add PhotoCard component

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,9 +1,10 @@
 import './PhotoList.css'
 import React from 'react'
+import PhotoCard from './PhotoCard'
 
 const PhotoList = props => {
   const photos = props.photos.map(photo => {
-    return <img src={photo.urls.thumb} alt={photo.description} key={photo.id} />
+    return <PhotoCard key={photo.id} photo={photo} />
   })
 
   return <div className="photo-list">{photos}</div>
