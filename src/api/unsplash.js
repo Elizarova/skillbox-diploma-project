d4df0ef01d53441df9bb2e8a7f9e4529abf0092d
elizarova <yelizarova.ae@gmail.com>
2019-01-21 18:20:13 +0300

    change Redirect URI

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -14,7 +14,7 @@ const unsplash = new Unsplash({
   applicationId:
     '6cd84a3d39af26340b81c3e1ed7158e197f6376882bc019f62073d738f10d52c',
   secret: '9498ce802640ef4d0962391bffc6911793bb54a32bed9b0a253f24b8fbdaacb0',
-  callbackUrl: 'https://skillbox-diploma-project.herokuapp.com/',
+  callbackUrl: 'https://skillbox-diploma-project.herokuapp.com/auth',
 })
 
 export default unsplash
